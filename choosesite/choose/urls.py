cec62d3d5190046d6ea4aa79b32682c64b14f7b2
Aya Maguire <amaguire@smartasset.com>
2019-06-09 16:05:27 -0400

    Got books, questions, and following rendered.

--- choosesite/choose/urls.py
+++ choosesite/choose/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'choose'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:book_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:book_id>/questions/', views.questions, name='questions'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/follow/', views.follow, name='follow'),
+    path('book/<int:book_id>/', views.detail, name='detail'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('question/<int:question_id>/follow/', views.follow, name='follow'),
 ]
--- choosesite/choose/views.py
+++ choosesite/choose/views.py
@@ -1,27 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Book
+from .models import Book, Question, Choice
 
 
 def index(request):
     book_list = Book.objects.all()
-    template = loader.get_template('choose/index.html')
     context = {
         'book_list': book_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'choose/index.html', context)
 
 
 def detail(request, book_id):
-    return HttpResponse("You're looking at book %s." % book_id)
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'choose/detail.html', {'book': book})
 
 
-def questions(request, book_id):
-    response = "You're looking at the questions of question %s."
-    return HttpResponse(response % book_id)
+def question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'choose/question.html', {'question': question})
 
 
 def follow(request, question_id):
-    return HttpResponse("You're following a choice on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'choose/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        next_question = selected_choice.directs_to
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('choose:question', args=(next_question.id,)))
