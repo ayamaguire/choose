f26a41efe1f192d32befd0e46e0b93c1a4639a0a
Aya Maguire <amaguire@smartasset.com>
2019-06-08 23:07:42 -0400

    Add url patterns.

--- choosesite/choosesite/settings.py
+++ choosesite/choosesite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'choose.apps.ChooseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
